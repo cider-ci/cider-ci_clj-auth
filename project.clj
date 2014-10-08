fe036f9bf00d288a2faa3983aa82a16e263924ed
Thomas Schank <DrTom@schank.ch>
2014-10-08 19:10:06 +0200

    Fix org.clojure/data.codec dep, upgrade deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider-ci/clj-auth "0.1.4"
+(defproject cider-ci/clj-auth "0.1.5"
   :description "Cider-CI Authentication"
   :url "https://github.com/cider-ci/cider-ci_clj-auth"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
@@ -6,9 +6,10 @@
   :dependencies [
                  [clj-logging-config "1.9.12"]
                  [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.codec "0.1.0"]
                  [org.clojure/java.jdbc "0.3.5"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [pandect "0.3.4"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [pandect "0.4.0"]
                  [robert/hooke "1.3.0"]
                  ]
   :source-paths ["src"]
