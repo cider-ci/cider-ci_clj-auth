f58d1d19187a49de2a0f7855b1589badf95e945e
Thomas Schank <DrTom@schank.ch>
2015-04-28 20:27:39 +0200

    Upgrade dependencies; set version to 3.0.0-rc.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject cider-ci/clj-auth "3.0.0-beta.7"
+(defproject cider-ci/clj-auth "3.0.0-rc.1"
   :description "Cider-CI Authentication"
   :url "https://github.com/cider-ci/cider-ci_clj-auth"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cider-ci/clj-utils "3.0.0-beta.6"]
-                 [cider-ci/open-session "1.1.0-beta.3"]
+                 [cider-ci/clj-utils "3.0.0-rc.1"]
+                 [cider-ci/open-session "1.1.0"]
                  ]
   :source-paths ["src"]
 
